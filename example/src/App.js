3488a5b8e8f6530ce1a6ed3f89f5a0f04ac3a045
Samet Topal <astopaal@gmail.com>
2023-03-28 13:51:33 +0300

    create 5 different button types

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'cutetons'
+import { Button } from 'cutetons'
 import 'cutetons/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+    <Button type="dashed" text="Yeni Button"></Button>
+    </>
+  )
+  
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from 'react';
+import styles from './styles.module.css';
+import PropTypes from 'prop-types';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+const Button = ({ type, children, text, ...rest }) => {
+  const buttonType = styles[`button-${type}`];
+
+  return (
+    <button className={`${styles.button} ${buttonType}`} {...rest}>{text}</button>
+  );
+};
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['primary', 'default', 'dashed', 'text', 'link']).isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+Button.defaultProps = {
+  type: 'default',
+};
+
+export {Button};
